515640233e3756ac1cbd2555fd182eaa77ccdd15
salsabila kartika <salsabilkrt@gmail.com>
2024-08-01 01:43:08 +0700

    Layout

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
-// Ini Javascript
+function formValidation() {
+    let name = document.getElementById('input').value;
+    let email = document.getElementById('email').value;
+
+    console.log(name);
+
+    // pengecekkan Nama dan email tidak boleh ''
+    if (name, email == '') {
+        // Code ini akan dieksekusi ketika name/email = ''
+        alert('Nama dan Email Tidak boleh kosong');
+    } else {
+        // Code ini akan dieksekusi ketika name/email tidak = ''
+        alert('Sukses menginput');
+    }
+}
+
+let indexSlide = 1; //[0, 1, 2]
+showSlide();
+
+function nextSlide(n) {
+    showSlide(indexSlide += n)
+
+}
+
+function showSlide(n) {
+    let listimage = document.getElementsByClassName('main-content-banner');
+    console.log(listimage);
+
+    // Algoritma untuk mereset index dari slidenya
+    if(n > listimage.length) indexSlide = 1;
+
+    // Algoritma untuk menghilangkan semua gambar
+    let index = 0;
+    while(index < listimage.length) {
+        listimage[index].style.display = 'none';
+        index++;
+    }
+
+    // Algoritma untuk memunculkan satu gambar
+    listimage[indexSlide -1].style.display = 'block';
+}
+
+setInterval(() => nextSlide(1), 3000)
